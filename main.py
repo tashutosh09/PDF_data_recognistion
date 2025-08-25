abcbee55232c21824595cf17223e003146bd70a0
Ashutosh Kumar <ashutoshkumar@MacBookAir.lan>
2025-08-25 12:50:07 +0530

    changed main

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from pdf_parser import pdf_to_markdown_pymupdf4llm
 if __name__ == "__main__":
     
     #upload button should call below function and generate output.md 
-    pdf_to_markdown_pymupdf4llm("raw_data/Anish_BigData_Engineer_Resume.pdf", output_path="stagig_data/output.md", write_images=True)
+    pdf_to_markdown_pymupdf4llm("raw_data/your_resume_filename.pdf", output_path="stagig_data/output.md", write_images=True)
     markdown_data = read_file_robust("stagig_data/output.md")
     input_topic = get_user_topic() #ask user for topic with validationstrip()
     if not input_topic:
